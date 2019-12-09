e8af7899934624f8e8a0affec61abac8e4390040
Wowo10 <wojtekwojtek529@gmail.com>
2019-12-09 19:44:08 +0100

    Implemented part 2 for day2.

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -3,12 +3,31 @@ use functions::file_utils::*;
 use functions::intcodes::*;
 
 fn main() {
-    let mut input = get_input();
+    let input_dupa = get_input();
+    //part1
+    println!("Result: {}", count_result(&input_dupa, 12, 2));
+    //part2
+    const DESIREDVALUE: i32 = 19690720;
 
-    change_value(&mut input, 1, 12);
-    change_value(&mut input, 2, 2);
+    for i in 0..=99 {
+        for j in 0..=99 {
+            let result = count_result(&input_dupa, i, j);
+            if result == DESIREDVALUE {
+                println!("Noun:{}, Verb:{}", i, j);
+                println!("Result:{}", 100 * i + j);
+                return;
+            }
+        }
+    }
+}
+
+fn count_result(input: &Vec<i32>, noun: i32, verb: i32) -> i32 {
+    let mut input = input.to_owned();
+
+    change_value(&mut input, 1, noun);
+    change_value(&mut input, 2, verb);
 
     handle_intcode(&mut input);
 
-    println!("Result: {}", get_value(&input, 0));
+    get_value(&input, 0)
 }
